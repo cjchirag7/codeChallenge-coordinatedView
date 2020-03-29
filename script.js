af0529ae600914f1d3bee0b42dc9f9fdd653158f
Chirag Jain <cjchirag7@gmail.com>
2020-03-29 18:10:18 +0530

    Implemented Sync while zooming, panning and rotation

--- script.js
+++ script.js
@@ -44,12 +44,21 @@ let rightSlider = document.getElementById('rightRotateSlider');
 let syncToggler = document.getElementById('sync_toggler');
 let initialLeftAngle = +leftSlider.value;
 let initialRightAngle = +rightSlider.value;
+let initialLeftCenter = leftViewer.viewport.getCenter();
+let initialRightCenter = rightViewer.viewport.getCenter();
+let initialLeftZoom = leftViewer.viewport.getZoom();
+let initialRightZoom = rightViewer.viewport.getZoom();
+let sync;
 
 function toggleSync() {
-  let sync = syncToggler.value;
+  sync = syncToggler.checked;
   if (sync) {
     initialLeftAngle = +leftSlider.value;
     initialRightAngle = +rightSlider.value;
+    initialLeftCenter = leftViewer.viewport.getCenter();
+    initialRightCenter = rightViewer.viewport.getCenter();
+    initialLeftZoom = leftViewer.viewport.getZoom();
+    initialRightZoom = rightViewer.viewport.getZoom();
   }
 }
 
@@ -57,7 +66,6 @@ function rotateLeftSlide() {
   let angle = +leftSlider.value;
   leftViewer.viewport.setRotation(angle);
   leftAngleDisplay.innerHTML = `${angle} deg.`;
-  let sync = syncToggler.checked;
   if (sync) {
     let angleDifference = angle - initialLeftAngle;
     let rightAngle = (initialRightAngle + angleDifference + 360) % 360;
@@ -71,7 +79,6 @@ function rotateRightSlide(e) {
   let angle = +rightSlider.value;
   rightViewer.viewport.setRotation(angle);
   rightAngleDisplay.innerHTML = `${angle} deg.`;
-  let sync = syncToggler.checked;
   if (sync) {
     let angleDifference = angle - initialRightAngle;
     let leftAngle = (initialLeftAngle + angleDifference + 360) % 360;
@@ -80,3 +87,53 @@ function rotateRightSlide(e) {
     leftAngleDisplay.innerHTML = `${leftAngle} deg.`;
   }
 }
+
+let leftViewerLeading = false;
+let rightViewerLeading = false;
+
+function leftHandler() {
+  if (rightViewerLeading) {
+    return;
+  }
+
+  if (sync) {
+    leftViewerLeading = true;
+    let zoomRatio = leftViewer.viewport.getZoom() / initialLeftZoom;
+    let finalZoom = zoomRatio * initialRightZoom;
+    rightViewer.viewport.zoomTo(finalZoom);
+    deltaX = leftViewer.viewport.getCenter().x - initialLeftCenter.x;
+    deltaY = leftViewer.viewport.getCenter().y - initialLeftCenter.y;
+    let targetCenter = new OpenSeadragon.Point(
+      initialRightCenter.x + deltaX,
+      initialRightCenter.y + deltaY
+    );
+    rightViewer.viewport.panTo(targetCenter);
+    leftViewerLeading = false;
+  }
+}
+
+function rightHandler() {
+  if (leftViewerLeading) {
+    return;
+  }
+
+  if (sync) {
+    rightViewerLeading = true;
+    let zoomRatio = rightViewer.viewport.getZoom() / initialRightZoom;
+    let finalZoom = zoomRatio * initialLeftZoom;
+    leftViewer.viewport.zoomTo(finalZoom);
+    deltaX = rightViewer.viewport.getCenter().x - initialRightCenter.x;
+    deltaY = rightViewer.viewport.getCenter().y - initialRightCenter.y;
+    let targetCenter = new OpenSeadragon.Point(
+      initialLeftCenter.x + deltaX,
+      initialLeftCenter.y + deltaY
+    );
+    leftViewer.viewport.panTo(targetCenter);
+    rightViewerLeading = false;
+  }
+}
+
+leftViewer.addHandler('pan', leftHandler);
+leftViewer.addHandler('zoom', leftHandler);
+rightViewer.addHandler('zoom', rightHandler);
+rightViewer.addHandler('pan', rightHandler);
